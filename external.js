23be77c928bfb32588dba75b6d78627e1484b738
Liztek <Elizabethtek15@gmail.com>
2023-04-18 13:24:23 -0500

    update

---
+++ external.js
@@ -0,0 +1,9 @@
+function calc() {
+    var out = document.getElementById("out");
+    out.Innertext = "Hello";
+}
+
+function reset() {
+    var out = document.getElementById("out");
+    out.Innertext = "";
+}
