2785b6008e724529354c3885d4cdc73186f949f8
Liztek <123410044+Liztek@users.noreply.github.com>
2023-05-05 13:41:10 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-function calc() {
-    var out = document.getElementById("reset").innerHTML = "reset"
-}
-
 function reset() {
+    var p = document.getElementById("0ut");
+    p.innerText = "";
+}
+function price() {
+    var n = 12
+    var p = Number(document.getElementById("p").value)
+    var r = Number(document.getElementById("r").value)
+     var t = Number(document.getElementById("t").value)
+    var m= Number(document.getElementById("m").value)
+    var B = p * (1 + r / n)** (n * t) - (m * ((1+ r/n)** (n * t)- 1)/ (r / n));
+    document.getElementById("out").innerHTML="$" + B.toFixed(2);
     
+
 }
