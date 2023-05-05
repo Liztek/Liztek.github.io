d456042dadcbbff4a59d8fa969a29501edc3b134
Liztek <Elizabethtek15@gmail.com>
2023-05-05 13:37:54 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 function calc() {
-    var out = document.getElementById("reset").innerHTML = "reset"
+    <p ide="out"></p>
+    var out = document.getElementById("calcualte").innerHTML = "calcualte"
+    var out = document.getElementById("calcualte").innerHTML = "calcualte"
+    var out = document.getElementById("calcualte").innerHTML = "calcualte"
+
 }
 
 function reset() {
-    
+    <p ide="out"></p>
+    var out = document.getElementbyId("reset").innerHTML = "reset"
+    var out = document.getElementbyId("reset").innerHTML = "reset"
+    var out = document.getElementbyId("reset").innerHTML = "reset"
+
+     
 }
+    
